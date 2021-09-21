09382fd249ae68073c284cbe26567d44cb99b2ff
RifqySyah16 <90077778+RifqySyah16@users.noreply.github.com>
2021-09-21 12:52:02 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "<p>Nama saya Muhammad Rifqy Syah</p>
+        <p>Saya tinggal di Jakarta Timur</p>
+       <p> Hobi saya adalah Ngoding</p>";
+    }
+}
