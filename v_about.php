b0fa868650f101fc433cf73db759ca6f661bbdee
RifqySyah16 <90077778+RifqySyah16@users.noreply.github.com>
2021-10-10 23:25:26 +0700

    Pertemuan 4

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Muhammad</li>
+        <li>Nama Belakang : Rifqy Syah</li>
+        <li>Nama Lengkap : Muhammad Rifqy Syah</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="square">
+        <li> Jl.Pondok Bambu Asri Selatan II No.34 Jakarta Timur</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="square">
+        <li> Jakarta, 16 September 2001</li>
+    </ul>
+    <h4>Hobby</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>Belajar hal-hal baru</li>
+    </ul>
+</section>
